36bc8108034c63204b435d3fdd72da4b628e4563
Andreto <andreas.o.tornkvist@gmail.com>
2019-03-27 18:47:26 +0100

    auto text color [not done]

--- extension/content.js
+++ extension/content.js
@@ -1,13 +1,14 @@
 console.log("running content.js")
 
-chrome.storage.sync.get(['highlightColor', 'highlightOnOff'], function(result) {
+chrome.storage.sync.get(['highlightColor', 'highlightOnOff', 'highlightTextColor'], function(result) {
   console.log('highlightColor currently is ' + result.highlightColor);
+  console.log('highlightTextColor currently is ' + result.highlightTextColor);
   if (result.highlightOnOff) {
     var element = document.createElement('style');
     element.setAttribute('id','highlightColorStyleElem');
     element.innerHTML =
-    '::selection {background: ' + result.highlightColor + '; color: #fff;}' +
-    '::-moz-selection {background: ' + result.highlightColor + '; color: #fff;}';
+    '::selection {background: ' + result.highlightColor + '; color: ' + result.highlightTextColor + ';}' +
+    '::-moz-selection {background: ' + result.highlightColor + '; color: ' + result.highlightTextColor + ';}';
     document.head.appendChild(element);
   }
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,7 @@
 let highlightInput = document.getElementById('highlight-input');
 let highlightTextInput = document.getElementById('highlight-text-input');
 let highlightOnOff = document.getElementById('highlight-on-off');
+let highlightAutoTextColor = document.getElementById('highlight-auto-text')
 
 function hexToRgb(hex) {
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
@@ -16,37 +17,24 @@ function hexToRgb(hex) {
     } : null;
 }
 
-function autoTextColorSet() {
-  chrome.storage.sync.get(['highlightColor'], function(result) {
-    var rgb = hexToRgb(result.highlightColor);
-    if ((rgb.r + rgb.g + rgb.b)/3 > 128){
-      console.log("ljus");
-    }else{
-      console.log("mörk");
-    }
+function inputUpdate(inputElem, colorElem, standardColor, storageKey){
+  var color = inputElem.value.replace("#", "");
+  if (color.length == 6 || color.length == 3){
+    color = "#" + color;
+  }else{
+    color = standardColor;
+  }
+  document.getElementById(colorElem).style.background = color;
+  inputElem.value = color;
+  var storageObj = {};
+  storageObj[storageKey] = color;
+  chrome.storage.sync.set(storageObj, function() {
+    console.log(colorElem + 'is set to ' + color);
   });
-}
-
-function setInputOnchange(inputElem, colorElem, standardColor, storageKey){
-  inputElem.onchange = function() {
-    var color = inputElem.value.replace("#", "");
-    if (color.length == 6 || color.length == 3){
-      color = "#" + color;
-    }else{
-      color = standardColor;
-    }
-    document.getElementById(colorElem).style.background = color;
-    inputElem.value = color;
-    var storageObj = {};
-    storageObj[storageKey] = color;
-    chrome.storage.sync.set(storageObj, function() {
-      console.log('highlightColor is set to ' + color);
-    });
-  };
 };
 
-setInputOnchange(highlightInput, 'highlight-color', '#ff80e4', 'highlightColor');
-setInputOnchange(highlightTextInput, 'highlight-text-color', '#ffffff', 'highlightTextColor');
+highlightInput.onchange = function() {inputUpdate(highlightInput, 'highlight-color', '#ff80e4', 'highlightColor');autoTextColorSet();};
+highlightTextInput.onchange = function() {inputUpdate(highlightTextInput, 'highlight-text-color', '#ffffff', 'highlightTextColor');};
 
 highlightOnOff.onchange = function() {
   chrome.storage.sync.set({highlightOnOff: highlightOnOff.checked}, function() {});
@@ -69,4 +57,31 @@ chrome.storage.sync.get(['highlightColor', 'highlightTextColor', 'highlightOnOff
   updatePopupValues(highlightInput, 'highlight-color', '#ff80e4', 'highlightColor', result.highlightColor);
   updatePopupValues(highlightTextInput, 'highlight-text-color', '#ffffff', 'highlightTextColor', result.highlightTextColor);
   highlightOnOff.checked = result.highlightOnOff;
- });
+});
+
+function autoTextColorSet() {
+  chrome.storage.sync.get(['highlightColor', 'highlightAutoTextColor', 'highlightTextColor'], function(result) {
+    if (result.highlightAutoTextColor) {
+      var rgb = hexToRgb(result.highlightColor);
+      if ((rgb.r + rgb.g + rgb.b)/3 > 128){
+        highlightTextInput.value = '#000000'
+        console.log("highlight-text-color auto-set: #0")
+      }else{
+        highlightTextInput.value = '#ffffff'
+        console.log("highlight-text-color auto-set: #f")
+      };
+      document.getElementById('highlight-text-color').style.background = highlightTextInput.value;
+      if (result.highlightTextColor !== highlightTextInput.value){
+        chrome.storage.sync.set({highlightTextInput: highlightTextInput.value}, function() {});
+      }
+    };
+    highlightTextInput.disabled = result.highlightAutoTextColor;
+    highlightAutoTextColor.checked = result.highlightAutoTextColor;
+  });
+}
+
+autoTextColorSet();
+highlightAutoTextColor.onchange = function() {
+  chrome.storage.sync.set({highlightAutoTextColor: highlightAutoTextColor.checked}, function() {});
+  autoTextColorSet();
+}
