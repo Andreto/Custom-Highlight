9f855610eab2981c8eca3b4fdf484f15f16e9a5a
Andreas Törnkvist <andreas.o.tornkvist@gmail.com>
2020-02-14 19:12:29 +0100

    google docs suport

--- extension/content.js
+++ extension/content.js
@@ -7,8 +7,9 @@ chrome.storage.sync.get(['highlightColor', 'highlightOnOff', 'highlightTextColor
     var element = document.createElement('style');
     element.setAttribute('id','highlightColorStyleElem');
     element.innerHTML =
-    '::selection {background: ' + result.highlightColor + '; color: ' + result.highlightTextColor + ';}' +
-    '::-moz-selection {background: ' + result.highlightColor + '; color: ' + result.highlightTextColor + ';}';
+    '::selection {background:' + result.highlightColor + '; color:' + result.highlightTextColor + ';}' +
+    '::-moz-selection {background:' + result.highlightColor + '; color:' + result.highlightTextColor + ';}' +
+    '.kix-selection-overlay {background: ' + result.highlightColor + ';color:' + result.highlightTextColor + ';border-top-color:' + result.highlightColor + ';border-bottom-color:' + result.highlightColor + ';}';
     document.head.appendChild(element);
   }
 });
